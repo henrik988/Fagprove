2d85eb5d32d22351502dfda514b73653a44c6a5a
unknown <hengun@BDN-1421.hq.bringdialog.no>
2015-09-22 19:41:18 +0200

    Fixed Particles.js!! :D

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,15 @@
 ﻿
-particlesJS.load('particles', 'src/js/cfg/particles.json', function () {
-    console.log('callback - particles.js config loaded');
-});
+
+
+
+var heroOverlay = $('.hero-overlay').height();
+$('.particles-js-canvas-el').attr('style', 'height:' + heroOverlay + ' !important');
+//$('.particles-js-canvas-el').css('height', heroOverlay, 'important');
+loadPartoclesJs()
+
+
+function loadPartoclesJs() {
+    particlesJS.load('particles', 'src/js/lib/cfg/particles.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+};
