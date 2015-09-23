c8d13bd71a202f4e0531f53e0d41bd9e8a0f23f5
unknown <hengun@BDN-1421.hq.bringdialog.no>
2015-09-23 19:44:44 +0200

    Added mobile menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,26 @@
-﻿
-
-$(function () {
-    // Initialize Particles JS
+﻿$(function () {
+    // Initialize Particles JS Hero 1
     particlesJS.load('particles', 'src/js/lib/cfg/particles.json', function () {
         console.log('callback - particles.js config loaded');
     });
 
-
     particlesJS.load('particles-quotes', 'src/js/lib/cfg/particles.json', function () {
         console.log('callback - particles.js config loaded');
     });
-
-
 
     // Config and initialize Early-bird countdown
     $('.countdown-text').countdown({
         date: "October 28, 2015 23:59:59"
     });
 
-
+    // Hamburger-menu animation toggle
+    $('#hamburger-menu').click(function () {
+        $(this).toggleClass('open');
+    });
 
 });
+
+// Toggle mobile menu
+function toggleMainMenu() {
+    $(".main-menu").toggleClass("toggled");
+};
