80ba7517064c80ad0e3c636a008546e8beeff4ba
unknown <hengun@BDN-1421.hq.bringdialog.no>
2015-09-23 12:44:23 +0200

    Added last sections UI + style.

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,18 @@ $(function () {
         console.log('callback - particles.js config loaded');
     });
 
+
+    particlesJS.load('particles-quotes', 'src/js/lib/cfg/particles.json', function () {
+        console.log('callback - particles.js config loaded');
+    });
+
+
+
     // Config and initialize Early-bird countdown
     $('.countdown-text').countdown({
         date: "October 28, 2015 23:59:59"
     });
+
+
 
 });
