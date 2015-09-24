af67ee9b1244c684636da8535557d8be18120083
unknown <hengun@BDN-1421.hq.bringdialog.no>
2015-09-24 12:39:13 +0200

    Simplified app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,19 @@
 ﻿$(function () {
+    var hero1 = 'particles';
+    var hero2 = 'particles-quotes';
+
     // Initialize Particles JS Hero 1
-    particlesJS.load('particles', 'src/js/lib/cfg/particles.json', function () {
+    particlesJS.load(hero1, 'src/js/lib/cfg/particles.json', function () {
         console.log('callback - particles.js config loaded');
     });
 
-    particlesJS.load('particles-quotes', 'src/js/lib/cfg/particles.json', function () {
-        console.log('callback - particles.js config loaded');
-    });
+    //Check if  Particles JS Hero 2 exists
+    if ($('#' + hero2).length) {
+        // Initialize Particles JS Hero 2
+        particlesJS.load(hero2, 'src/js/lib/cfg/particles.json', function () {
+            console.log('callback - particles.js config loaded');
+        });
+    }
 
     // Config and initialize Early-bird countdown
     $('.countdown-text').countdown({
