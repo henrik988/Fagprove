56337621b00faa85e4442f1d8d017eaff72435cc
unknown <hengun@BDN-1421.hq.bringdialog.no>
2015-09-24 11:47:36 +0200

    Fixed routing + added new page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	include_once('templ/inc/header.html');
 
 	if($_GET['page'] != '' && file_exists('templ/' . $_GET['page'] . '.html')){
-		include_once('pages/' . $_GET['page'] . '.html');
+		include_once('templ/' . $_GET['page'] . '.html');
 	} else {
 		include_once('templ/index.html');
 	}
