319f288e0ab899f48d154e26e5fc518cfd3e2a3c
unknown <hengun@BDN-1421.hq.bringdialog.no>
2015-09-24 16:06:09 +0200

    Added handler for  "Select tickets toggle section" + changed countdown JS-class

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 ﻿$(function () {
+
     var hero1 = 'particles';
     var hero2 = 'particles-quotes';
 
@@ -7,7 +8,7 @@
         console.log('callback - particles.js config loaded');
     });
 
-    //Check if  Particles JS Hero 2 exists
+    // Check if  Particles JS Hero 2 exists
     if ($('#' + hero2).length) {
         // Initialize Particles JS Hero 2
         particlesJS.load(hero2, 'src/js/lib/cfg/particles.json', function () {
@@ -16,7 +17,7 @@
     }
 
     // Config and initialize Early-bird countdown
-    $('.countdown-text').countdown({
+    $('.countdown').countdown({
         date: "October 28, 2015 23:59:59"
     });
 
@@ -24,6 +25,23 @@
     $('#hamburger-menu').click(function () {
         $(this).toggleClass('open');
         $(".main-menu").toggleClass("toggled");
+    });
+
+    // Select tickets toggle section
+    ticketsToggleClass = '.select-tickets-header';
+    $(ticketsToggleClass).click(function () {
+        $(ticketsToggleClass).parent().removeClass('toggled');
+        $(this).parent().addClass('toggled');
+
+        // Close the one you NOT clicked on
+        $(ticketsToggleClass).each(function () {
+            if ($(this).parent().hasClass('toggled')) {
+                $(this).next().slideDown();
+            } else {
+                $(this).next().slideUp();
+            };
+        });
+
     });
 
 });
